741c7e92cbc06d7c280585a1e3c9a330c08a4a4c
Florencia-97 <florrr1997@gmail.com>
2020-10-04 18:33:15 -0300

    Arreglo impresión de meses

--- src/utils/mes.js
+++ src/utils/mes.js
@@ -4,5 +4,7 @@ const nombresMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
 "Noviembre", "Diciembre"];
 
 export default function(numMes){
-    return nombresMeses[numMes]
+    var pos = numMes -1;
+    if (pos === 12){pos = 0}
+    return nombresMeses[pos];
 }
